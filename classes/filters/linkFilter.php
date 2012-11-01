9dae4f20c2c5533ad172d4787d0f4e5a457c19a5
Jeremy Dorn <jeremy@jeremy.(none)>
2012-11-01 11:27:09 -0700

    Making the link filter ignore null values

--- classes/filters/linkFilter.php
+++ classes/filters/linkFilter.php
@@ -1,6 +1,8 @@
 <?php
 class linkFilter extends FilterBase {	
 	public static function filter($value, $options = array(), $report=null) {
+		if(!$value->getValue()) return $value;
+
 		$html = '<a href="'.$value->getValue().'"'.($options['blank']? ' target="_blank"':'').'>'.(isset($options['display'])? $options['display'] : $value->getValue(true)).'</a>';
 		$value->setValue($html, true);
 		
