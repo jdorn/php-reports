ca7e63fc8a292e5a8fc134500ca88f73314106b3
Jeremy Dorn <jeremy@jeremydorn.com>
2014-03-05 21:25:35 -0800

    Fix accidental revert of #124

--- lib/PhpReports/PhpReports.php
+++ lib/PhpReports/PhpReports.php
@@ -268,7 +268,7 @@ class PhpReports {
 	public static function getDashboard($dashboard) {
 		$file = PhpReports::$config['dashboardDir'].'/'.$dashboard.'.json';
 		if(!file_exists($file)) {
-			throw "Unknown dashboard - ".$dashboard;
+			throw new Exception("Unknown dashboard - ".$dashboard);
 		}
 		
 		return json_decode(file_get_contents($file),true);
