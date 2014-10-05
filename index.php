7fdd3f164e69abf6d6a7be678ca66bbdcb0cbc6b
Jeremy Dorn <jdorn@education.com>
2014-10-04 21:25:10 -0700

    Fix typos.

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ require 'lib/PhpReports/PhpReports.php';
 // Google Analytics API
 if(isset(PhpReports::$config['ga_api'])) {
   $ga_client = new Google_Client();
-  $ga_client->setApplicationName($config['ga_api']['applicationName']);
-  $ga_client->setClientId($config['ga_api']['clientId']);
-  $ga_client->setClientSecret($config['ga_api']['clientSecret']);
-  $ga_client->setRedirectUri($config['ga_api']['redirectUri']);
+  $ga_client->setApplicationName(PhpReports::$config['ga_api']['applicationName']);
+  $ga_client->setClientId(PhpReports::$config['ga_api']['clientId']);
+  $ga_client->setClientSecret(PhpReports::$config['ga_api']['clientSecret']);
+  $ga_client->setRedirectUri(PhpReports::$config['ga_api']['redirectUri']);
   $ga_service = new Google_Service_Analytics($ga_client);
   $ga_client->addScope(Google_Service_Analytics::ANALYTICS);
   if(isset($_GET['code'])) {
